860d384596a48eeed6d82455e31a3811471f9e3c
Askyc <askyv@outlook.com>
2020-06-09 22:53:40 +0800

    添加代码自动生成模块

---
+++ gouwo-codetool/src/main/java/com/gouwo/CodeGenerator.java
@@ -0,0 +1,152 @@
+package com.gouwo;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.core.exceptions.MybatisPlusException;
+import com.baomidou.mybatisplus.core.toolkit.StringPool;
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.InjectionConfig;
+import com.baomidou.mybatisplus.generator.config.*;
+import com.baomidou.mybatisplus.generator.config.po.TableInfo;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+import java.util.Scanner;
+
+public class CodeGenerator {
+
+    /**
+     * <p>
+     * 读取控制台内容
+     * </p>
+     */
+    public static String scanner(String tip) {
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder help = new StringBuilder();
+        help.append("请输入" + tip + "：");
+        System.out.println(help.toString());
+        if (scanner.hasNext()) {
+            String ipt = scanner.next();
+            if (StringUtils.isNotEmpty(ipt)) {
+                return ipt;
+            }
+        }
+        throw new MybatisPlusException("请输入正确的" + tip + "！");
+    }
+
+    public static void main(String[] args) {
+
+        //用来获取Mybatis-Plus.properties文件的配置信息
+        final ResourceBundle rb = ResourceBundle.getBundle("mybatis-plus");
+
+        AutoGenerator mpg = new AutoGenerator();
+
+        //全局配置
+        GlobalConfig gc = new GlobalConfig();
+        String projectPath = System.getProperty("user.dir");
+        //生成路径(一般都是生成在此项目的src/main/java下面)
+        gc.setOutputDir(projectPath +rb.getString("modulePath")+"/src/main/java");
+        //第二次生成会把第一次的覆盖
+        gc.setFileOverride(true);
+        //不需要ActiveRecord特性的请改为false
+        gc.setActiveRecord(false);
+        //XML二级缓存
+        gc.setEnableCache(false);
+        //XML ResultMap
+        gc.setBaseResultMap(true);
+        //XML columList  在xml中生产基础列
+        gc.setBaseColumnList(true);
+        gc.setOpen(false);
+        //作者
+        gc.setAuthor(rb.getString("author"));
+
+        //数据源配置
+        DataSourceConfig dsc = new DataSourceConfig();
+        dsc.setUrl(rb.getString("url"));
+        dsc.setDriverName(rb.getString("driverName"));
+        //数据库类型
+        dsc.setDbType(DbType.MYSQL);
+        dsc.setUsername("root");
+        dsc.setPassword("root");
+        mpg.setDataSource(dsc);
+
+        //自定义文件命名，注意%s 会自动填充表实体属性
+        gc.setControllerName("%sController");
+        gc.setServiceName("%sService");
+        gc.setServiceImplName("%sServiceImpl");
+        gc.setMapperName("%sMapper");
+        gc.setXmlName("%sMapper");
+        mpg.setGlobalConfig(gc);
+
+         //包配置
+        PackageConfig pc = new PackageConfig();
+        //pc.setModuleName(scanner("模块名"));
+        pc.setParent(rb.getString("parent"));
+        pc.setController("controller");
+        pc.setService("service");
+        pc.setServiceImpl("service"+ ".impl");
+        pc.setMapper("mapper");
+        pc.setEntity("model");
+        mpg.setPackageInfo(pc);
+
+        // 自定义配置
+        InjectionConfig cfg = new InjectionConfig() {
+            @Override
+            public void initMap() {
+                // to do nothing
+            }
+        };
+
+        // 如果模板引擎是 freemarker
+        String templatePath = "/templates/mapper.xml.ftl";
+
+        // 自定义输出配置
+        List<FileOutConfig> focList = new ArrayList<>();
+        // 自定义配置会被优先输出
+        focList.add(new FileOutConfig(templatePath) {
+            @Override
+            public String outputFile(TableInfo tableInfo) {
+                // 自定义输出文件名 ， 如果你 Entity 设置了前后缀、此处注意 xml 的名称会跟着发生变化！！
+                return projectPath + rb.getString("modulePath") + "/src/main/resources/mapper/"
+                        + "/" + tableInfo.getEntityName() + "Mapper" + StringPool.DOT_XML;
+            }
+        });
+        cfg.setFileOutConfigList(focList);
+        mpg.setCfg(cfg);
+
+        // 配置模板
+        TemplateConfig templateConfig = new TemplateConfig();
+        templateConfig.setXml(null);
+        mpg.setTemplate(templateConfig);
+
+        //策略配置
+        StrategyConfig strategy = new StrategyConfig();
+        //此处可以修改表前缀
+        strategy.setTablePrefix(new String[]{});
+        //表名生成策略,下划线到驼峰的命名规则
+        strategy.setNaming(NamingStrategy.underline_to_camel);
+        strategy.setColumnNaming(NamingStrategy.underline_to_camel);
+        //开启全局大写命名
+        strategy.setCapitalMode(true);
+        strategy.setEntityLombokModel(true);
+        strategy.setRestControllerStyle(true);
+        //需要生成的表,如果为空，则默认读取数据库的所有表名
+        //strategy.setInclude(new String[]{"t_user"});
+        strategy.setInclude(scanner("表名，多个英文逗号分割").split(","));
+        strategy.setControllerMappingHyphenStyle(true);
+        strategy.setTablePrefix(pc.getModuleName() + "_");
+        strategy.setSuperServiceClass(null);
+        strategy.setSuperServiceImplClass(null);
+        strategy.setSuperMapperClass(null);
+        mpg.setStrategy(strategy);
+
+        // set freemarker engine
+        mpg.setTemplateEngine(new FreemarkerTemplateEngine());
+        //执行生成
+        mpg.execute();
+    }
+
+}
