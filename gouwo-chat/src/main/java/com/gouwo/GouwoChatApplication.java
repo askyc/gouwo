5c47b2ce4b04260a070d6ae5b99e346f4b570423
Askyc <askyv@outlook.com>
2021-04-17 21:16:24 +0800

    add chat

---
+++ gouwo-chat/src/main/java/com/gouwo/GouwoChatApplication.java
@@ -0,0 +1,18 @@
+package com.gouwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: asky
+ * @Date: 2021/3/5 18:29
+ * @Desc:
+ */
+@SpringBootApplication
+public class GouwoChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GouwoChatApplication.class, args);
+    }
+
+}
